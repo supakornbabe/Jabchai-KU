e1cfb671614020ddb86843f3038d509f573341e5
supakornbabe <supakornbabew@hotmail.com>
2016-10-09 13:32:51 +0700

    add bootstrap

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>TEST Webpage</title>
+</head>
+<body>
+
+      <?php
+           $myName = "Eric";
+           $myAge=18;
+           echo $myName;
+      ?>
+
+</body>
+</html>
